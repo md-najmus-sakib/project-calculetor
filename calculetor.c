98bfe402eb7f1fc074ac1df5fdad5b92e1413e72
sakib7495 <najmussakib0066@gmail.com>
2022-10-01 23:52:32 +0600

    Se

--- calculetor.c
+++ calculetor.c
@@ -8,35 +8,39 @@ int main()
     printf("----------------------------------------------------\n");
     printf("Select Operator:\n");
     printf("Addition (+), Substraction (-), Multiplition (*), Division (/):\n");
-    
+
     char ope[5];
-    scanf("%c", &ope[0]);
     float a, b;
     double sum, sub, mul, div;
 
-    if (ope[0] == '+')
+    while (1)
     {
-        scanf("%f%f", &a, &b);
-        sum = a + b;
-        printf("Result is: %.4lf\n", sum);
-    }
-    else if (ope[0] == '-')
-    {
-        scanf("%f%f", &a, &b);
-        sub = a - b;
-        printf("Result is: %.4lf\n", sub);
-    }
-    else if (ope[0] == '*')
-    {
-        scanf("%f%f", &a, &b);
-        mul = a * b;
-        printf("Result is: %.4lf\n", mul);
-    }
-    else if (ope[0] == '/')
-    {
-        scanf("%f%f", &a, &b);
-        div = a / b;
-        printf("Result is: %.4lf\n", div);
+        scanf("%c", &ope[0]);
+
+        if (ope[0] == '+')
+        {
+            scanf("%f%f", &a, &b);
+            sum = a + b;
+            printf("Result is: %.4lf\n", sum);
+        }
+        else if (ope[0] == '-')
+        {
+            scanf("%f%f", &a, &b);
+            sub = a - b;
+            printf("Result is: %.4lf\n", sub);
+        }
+        else if (ope[0] == '*')
+        {
+            scanf("%f%f", &a, &b);
+            mul = a * b;
+            printf("Result is: %.4lf\n", mul);
+        }
+        else if (ope[0] == '/')
+        {
+            scanf("%f%f", &a, &b);
+            div = a / b;
+            printf("Result is: %.4lf\n", div);
+        }
     }
 
     return 0;
